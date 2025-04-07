af26d41313e74fa0df901eff1b4c546f704740e8
P_C044 <P_C044@student.usc.edu.au>
2025-04-07 15:58:33 +1000

    GUIMAIN WEEK 7

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -8,7 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -19,13 +21,40 @@ import javafx.stage.Stage;
  */
 public class GuiMain extends Application {
 
+    private GardenPlanner planner;
+
+    public GuiMain() {
+        GardenPlanner planner = new GardenPlanner();
+        planner.createBasicDesign();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Parent root = FXMLLoader.load(getClass().getResource("garden_gui.fxml"));
-        Button root = new Button("TODO: design garden planner GUI");
+        Pane pane = new Pane();
+        pane.setStyle("-fx-background-color: #007700;");
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(100);
+        rectangle.setWidth(100);
+        rectangle.setLayoutX(200);
+        rectangle.setLayoutY(100);
+
+        Rectangle rectangleLeft = new Rectangle();
+        rectangleLeft.setHeight(100);
+        rectangleLeft.setWidth(50);
+        rectangleLeft.setLayoutY(100);
+        rectangleLeft.setLayoutX(100);
+
+        Rectangle rectangleRight = new Rectangle();
+        rectangleRight.setHeight(100);
+        rectangleRight.setWidth(50);
+        rectangleRight.setLayoutY(100);
+        rectangleRight.setLayoutX(350);
+
+        pane.getChildren().addAll(rectangleLeft, rectangle, rectangleRight);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(pane, 800, 600));
         primaryStage.show();
     }
 
@@ -33,4 +62,13 @@ public class GuiMain extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void defaultLayout() {
+        for (RectBed bed : planner.getBeds()) {
+
+        }
+
+
+    }
+
 }
--- src/test/java/RectBedTest.java
+++ src/test/java/RectBedTest.java
@@ -18,4 +18,25 @@ public class RectBedTest {
         assertEquals(6, rBed.getArea());
     }
 
+    @Test
+    void testGetPerimeter() {
+        RectBed rBed2 = new RectBed();
+
+        rBed2.setWidth(6);
+        rBed2.setHeight(7);
+        assertEquals(26, rBed2.getPerimeter());
+    }
+
+    @Test
+    void testToString()  {
+        RectBed rBed3 = new RectBed();
+
+        rBed3.setTop(2);
+        rBed3.setLeft(4);
+        rBed3.setWidth(5);
+        rBed3.setHeight(10);
+        assertEquals("Rectangle 4.00 2.00 5.00 10.00", rBed3.toString());
+
+    }
+
 }
